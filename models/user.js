0309cb4bc311586de1c9e3c6a8dc4f8e1f9eea35
Alex Yuan <alexyuan@lawn-128-61-29-141.lawn.gatech.edu>
2017-05-13 13:14:15 -0400

    test

--- models/user.js
+++ models/user.js
@@ -1,22 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    // Giving the Author model a name of type STRING
-    name: DataTypes.STRING
-  },
-    // Here we'll pass a second "classMethods" object into the define method
-    // This is for any additional configuration we want to give our models
-    {
-      // We're saying that we want our Author to have Posts
-      classMethods: {
-        associate: function(models) {
-          // Associating Author with Posts
-          // When an Author is deleted, also delete any associated Posts
-          User.hasMany(models.Post, {
-            onDelete: "cascade"
-          });
-        }
-      }
-    }
-  );
-  return User;
+   var User = sequelize.define("User", {
+       name: {
+           type: DataTypes.STRING,
+           allowNull: false,
+           validate: {
+               len: [6, 20]
+           }
+       },
+       email: {
+           type: DataTypes.STRING,
+           allowNull: false,
+           validate: {
+               len: [6, 20]
+           }
+       }
+   });
+   return User;
 };
