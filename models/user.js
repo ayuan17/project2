129b78bac7531c5470c4a0c6d1461a21fc5b7b77
Hanasandy <heiditochigi@gmail.com>
2017-05-15 15:56:23 -0400

    Heidi push on 5/15/17

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
    var User = sequelize.define("User", {
-       name: {
+       username: {
            type: DataTypes.STRING,
            allowNull: false,
            validate: {
                len: [6, 20]
-           }
+           },
+           unique: true
        },
        email: {
            type: DataTypes.STRING,
@@ -13,6 +14,10 @@ module.exports = function(sequelize, DataTypes) {
            validate: {
                len: [6, 20]
            }
+       },
+       password: {
+           type: DataTypes.STRING,
+           allownull: false
        }
    });
    return User;
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
+// for password authentication
+var jwt = require("jsonwebtoken");
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,12 +23,33 @@ app.use(express.static("./public"));
 
 // Routes =============================================================
 
-// require("./routes/html-route.js")(app);
-// require("./routes/api-route.js")(app);
+require("./routes/html-route.js")(app);
+var api = require("./routes/api-route.js");
 // require("./routes/profile-route.js")(app);
 
+
+app.use("/api", api);
+app.use("/api/secure", function (req, res, next) {
+  if (!req.header("Authorization")) {
+    res.status(401).json({ "status": "Not Authorized"});
+  } else {
+    jwt.verify(req.header("Authorization"), "randomsecretforsigningjwt", function(err, decoded) {
+      if (err) {
+        console.log("err", err)
+        res.status(401).json({ "status": "Not Authorized"});
+      } else {
+        console.log(decoded.data)
+        next();
+      }
+    });
+  }
+  // else res.status(401).json({})
+});
+app.use("/api/secure", api);
+
+
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
