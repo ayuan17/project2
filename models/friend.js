cfe851d9f2836c5483692f877b28d3d671f26b82
Alex Yuan <alexyuan@Alexs-MacBook-Pro.local>
2017-05-19 16:48:24 -0400

    test goals list

--- models/friend.js
+++ models/friend.js
@@ -1,13 +1,13 @@
 /**
  * Created by Jimmia Rowland.
  */
-module.exports = function(sequelize, DataTypes) {
-	var Friend = sequelize.define("Friend",{
-		name:{
-			type: DataTypes.STRING,
-			allowNull: false
-		}
-	});
+// module.exports = function(sequelize, DataTypes) {
+// 	var Friend = sequelize.define("Friend",{
+// 		name:{
+// 			type: DataTypes.STRING,
+// 			allowNull: false
+// 		}
+// 	});
 
-	return Friend;
-};
+// 	return Friend;
+// };
